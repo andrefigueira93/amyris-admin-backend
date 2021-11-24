f2d596834f3f585844a41318cbd0c83458434edf
André Figueira <andre.figueira@envolti.com.br>
2021-11-24 15:26:18 -0300

    Configuring Cache for Pages list route

--- app/Http/Controllers/Api/LandingPageController.php
+++ app/Http/Controllers/Api/LandingPageController.php
@@ -5,21 +5,25 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\LandingPage;
 use App\Models\Project;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class LandingPageController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      */
-    public function index(string $projectDomain)
+    public function index(string $projectDomain): JsonResponse
     {
-        $pages = Project::where('domain', $projectDomain)->first()
-            ->pages()
-            ->where('active', 1)
-            ->get();
+        $pages = \Cache::remember("projects:$projectDomain:pages", 36000, function () use ($projectDomain){
+            return Project::where('domain', $projectDomain)->first()
+                ->pages()
+                ->where('active', 1)
+                ->get();
+            });
         return response()->json($pages);
     }
 
@@ -27,9 +31,8 @@ class LandingPageController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request): void
     {
         //
     }
@@ -38,9 +41,9 @@ class LandingPageController extends Controller
      * Display the specified resource.
      *
      * @param string $slug
-     * @return \Illuminate\Http\JsonResponse
+     * @return JsonResponse
      */
-    public function show(string $slug): \Illuminate\Http\JsonResponse
+    public function show(string $slug): JsonResponse
     {
         $lp = \Cache::remember("pages:$slug", 3600, function () use ($slug) {
             return LandingPage::where('slug', $slug)->first();
@@ -52,10 +55,9 @@ class LandingPageController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, int $id): void
     {
         //
     }
@@ -63,10 +65,9 @@ class LandingPageController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param int $id
      */
-    public function destroy($id)
+    public function destroy(int $id): void
     {
         //
     }
